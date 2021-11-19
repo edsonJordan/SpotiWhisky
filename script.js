dba6a64fc0063eca241d21d26504702b5923f378
Edson Jordan <edson_4555@hotmail.com>
2021-11-18 20:52:10 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+
+
+document.getElementById('search-icon').addEventListener('change', (e) => {
+    document.getElementById("header").classList.toggle("header_active");
+});
+document.getElementById("navigation-icon").addEventListener("change", (e) => {
+    document.getElementById("ul__navigation").classList.toggle("ul__navigation--active");
+});
+
+
+
+const CLIENT_ID = "e666d111cbeb49f897e1a05352690b42"; // insert your client id here from spotify
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL_AFTER_LOGIN = "http://localhost:5500/";
+const SPACE_DELIMITER = "%20";
+const SCOPES = ["user-read-currently-playing",
+"user-read-playback-state",
+"playlist-read-private", 
+"user-read-playback-position",
+"user-read-recently-played",
+"user-top-read"];
+const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
+
+document.getElementById("Loggin")?.addEventListener("click", () => {
+    //window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=user-read-currently-playing%20user-top-read`;
+    window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+});
+const getReturnedParamsFromSpotifyAuth = (hash) => {
+    const stringAfterHashtag = hash.substring(1);
+    const paramsInUrl = stringAfterHashtag.split("&");
+    const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+    const [key, value] = currentValue.split("=");
+      accumulater[key] = value;
+      return accumulater;
+    }, {});
+    return paramsSplitUp;
+  };
+
+
+window.onload = function() {
+    if (localStorage.getItem("TokenCode")) {
+        //document.getElementById("Loggin").style.display = "none";
+    }else{
+        //document.getElementById("Loggin").style.display = "block";
+    }
+    restartToken(window.location.hash);
+    
+  };
+  function restartToken(hash) {
+      if (hash) {
+            const { access_token } =
+            getReturnedParamsFromSpotifyAuth(hash);
+            localStorage.clear();
+            localStorage.setItem("TokenCode", access_token);
+            window.location.href = REDIRECT_URL_AFTER_LOGIN;
+          }
+    
+  }
+    // private methods
+ 
+
+    /* const getToken = async () => {
+
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded', 
+                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+        const data = await result.json();
+        console.log(data);
+        return data.access_token;
+    }; */
+  
+
+
+  
+
+
+//getToken();
